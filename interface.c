093ba3b9b1fcb71a981b2b87bdc8026d889559fa
fanfanliye <114798280+fanfanliye@users.noreply.github.com>
2022-10-06 21:19:59 -0700

    Init main and implement interface

---
+++ interface.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "interface.h"
+
+
+void PrintStart() {
+  printf("###############################\n");
+  printf("Welcome to Tic-Tac-Toe!\n");
+  printf("The game will start!\n");
+}
+void PrintGameOver(char *winner) {
+  printf("Game Over!\n");
+  printf("Winner is: %s!", winner);
+}
+void PrintEnd() {
+  printf("Exiting game...\nDone!\n");
+}
+
---
+++ interface.h
@@ -0,0 +1,10 @@
+#ifndef TICTACTOEINC__INTERFACE_H_
+#define TICTACTOEINC__INTERFACE_H_
+
+#endif //TICTACTOEINC__INTERFACE_H_
+
+void PrintStart();
+
+void PrintGameOver(char* winner);
+
+void PrintEnd();
