e4f40cbee1c94990de67d6b086e160a9698e86f9
fanfanliye <114798280+fanfanliye@users.noreply.github.com>
2022-10-06 21:49:47 -0700

    Update main

--- interface.c
+++ interface.c
@@ -8,9 +8,9 @@ void PrintStart() {
   printf("Welcome to Tic-Tac-Toe!\n");
   printf("The game will start!\n");
 }
-void PrintGameOver(char *winner) {
+void PrintGameOver(char* winner) {
   printf("Game Over!\n");
-  printf("Winner is: %s!", winner);
+  printf("Winner is: %s!\n", winner);
 }
 void PrintEnd() {
   printf("Exiting game...\nDone!\n");
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+#include "interface.h"
+
 int main() {
-  printf("Hello, World!\n");
-  return 0;
+  PrintStart();
+  PrintGameOver("You");
+  PrintEnd();
 }
